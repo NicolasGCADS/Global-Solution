525de6bbd3e855e48a041f5d16799b7be123e802
NicolasGCADS <nguinante@gmail.com>
2025-06-03 22:11:49 -0300

    Começando Razor e TagHelpers

---
+++ SafeZoneMVP/Models/Alerta.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SafeZoneMVP.Pages
+{
+    public class PageAlertaModel
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataHora { get; set; }
+    }
+
+}
---
+++ SafeZoneMVP/Models/AlertaLogica.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+namespace SafeZoneMVP.Pages
+{
+    public class AlertasModel : PageModel
+    {
+        public List<PageAlertaModel> ListaAlertas { get; set; }
+
+        public void OnGet()
+        {
+            // Simula dados (você pode buscar de um banco real)
+            ListaAlertas = new List<PageAlertaModel>
+            {
+                new PageAlertaModel { Id = 1, Tipo = "Erro", Descricao = "Falha ao carregar módulo", DataHora = DateTime.Now },
+                new PageAlertaModel { Id = 2, Tipo = "Aviso", Descricao = "Atualização disponível", DataHora = DateTime.Now.AddMinutes(-10) }
+            };
+        }
+    }
+}
